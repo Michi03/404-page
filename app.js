26b8f5203574ea4b47b3ce36cc19057b402ff71c
Michi <mic.michael@gmx.de>
2020-02-23 20:28:20 +0100

    added statuses

--- app.js
+++ app.js
@@ -4,52 +4,52 @@ var app = express();
 app.get('/', function (req, res) {
   switch(req.headers["x-code"]) {
     case "200":
-      res.sendfile("views/index.html");
+      res.status(200).sendfile("views/index.html");
       break;
     case "400":
-      res.sendfile("views/400.html");
+      res.status(400).sendfile("views/400.html");
       break;
     case "401":
-      res.sendfile("views/401.html");
+      res.status(401).sendfile("views/401.html");
       break;
     case "403":
-      res.sendfile("views/403.html");
+      res.status(403).sendfile("views/403.html");
       break;
     case "404":
-      res.sendfile("views/404.html");
+      res.status(404).sendfile("views/404.html");
       break;
     case "405":
-      res.sendfile("views/405.html");
+      res.status(405).sendfile("views/405.html");
       break;
     case "408":
-      res.sendfile("views/408.html");
+      res.status(408).sendfile("views/408.html");
       break;
     case "410":
-      res.sendfile("views/410.html");
+      res.status(410).sendfile("views/410.html");
       break;
     case "418":
-      res.sendfile("views/418.html");
+      res.status(418).sendfile("views/418.html");
       break;
     case "500":
-      res.sendfile("views/500.html");
+      res.status(500).sendfile("views/500.html");
       break;
     case "501":
-      res.sendfile("views/501.html");
+      res.status(501).sendfile("views/501.html");
       break;
     case "502":
-      res.sendfile("views/502.html");
+      res.status(502).sendfile("views/502.html");
       break;
     case "503":
-      res.sendfile("views/503.html");
+      res.status(503).sendfile("views/503.html");
       break;
     case "504":
-      res.sendfile("views/504.html");
+      res.status(504).sendfile("views/504.html");
       break;
     case "511":
-      res.sendfile("views/511.html");
+      res.status(511).sendfile("views/511.html");
       break;
     default:
-      res.sendfile("views/404.html");
+      res.status(404).sendfile("views/404.html");
       break;
   }
 });
